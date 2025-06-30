55a1b0e15d0a91cd279180d375f9bbedac564832
dabartmess <dabartmess@proton.me>
2025-06-30 17:40:35 -0600

    Updates to get image embedded. Still isn't creating post from SP uri, so working on that.

--- src/bsky_SP_creator.py
+++ src/bsky_SP_creator.py
@@ -8,13 +8,15 @@ from atproto_client.models.blob_ref import BlobRef
 import atproto_core
 from bsky_follows_util import get_followers
 
+import session_reuse
+
 sp1_name = "Dingo Dave SP #"
 sp1_description = "Automated Starter Pack by THE Dingo Dave\nFollow these folks, they're great!"
 handle = 'thedingodave.substack.com'
 password = 'i3g7-27cm-oezl-dfsm'
 
-
-def create_SP_list(followers):
+def create_SP_list(followers, handle, password):
+    client = session_reuse.init_client(handle, password)
     sp_items = []
     max_sp = 150
     curr = 0
@@ -32,9 +34,9 @@ def create_SP_list(followers):
                 #print("follower DID: ", followers[curr+i])
 
         #print("SPNAME: ", sp1_name)
-        spuri = create_SP(sp_list, sp_num, handle, password)
+        spuri = create_SP(sp_list, sp_num, sp1_name, sp1_description, handle, password)
         #print("SP URI: ", spuri)
-        bsky_post = create_post(spuri, sp_num)
+        bsky_post = create_post(spuri, sp_num, handle, password)
         break
 
         sp_num += 1
@@ -43,13 +45,16 @@ def create_SP_list(followers):
     print("Number of items: ", len(sp_items))
     #print(SP_items)
 
-def create_post(spuri, sp_num):
+def create_post(spuri, sp_num, handle, password):
+    client = session_reuse.init_client(handle, password)
     print("Entering create_post ")
 
-    client = Client()
-    profile = client.login(handle, password)
+    client = session_reuse.init_client(handle, password)
+    # client = Client()
+    # profile = client.login(handle, password)
     resolver = atproto.IdResolver()
     did = resolver.handle.resolve('thedingodave.substack.com')
+
     img_bytes = None
     img_path = "/home/dabartmess/Dropbox/Protest/BlueSkyLiberty.jpg"
 
@@ -57,55 +62,59 @@ def create_post(spuri, sp_num):
 
     # ref2 = client.send_image(text="BlueSky Liberty", image=img_data, image_alt="BlueSky Liberty Logo")
     # print("Ref: ", ref2)
-    print("Creating embeds")
+    # print("Creating embeds")
 
-    with open(img_path, "rb") as f:
-        img_data = f.read()
-    blob_ref = atproto_client.models.blob_ref.BlobRef(
-        alt="BlueSky Liberty Wave",
-        mime_type="image/jpeg",
-        ref=spuri,
-        maxSize=1000000,
-        size=190055,
-        py_type="blob"
-    )
+    # with open(img_path, "rb") as f:
+    #     img_data = f.read()
+    # blob_ref = atproto_client.models.blob_ref.BlobRef(
+    #     alt="BlueSky Liberty Wave",
+    #     mime_type="image/jpeg",
+    #     ref=spuri,
+    #     maxSize=1000000,
+    #     size=190055,
+    #     py_type="blob"
+    # )
+    #
+    # print("Blob: ", blob_ref)
+    # print("URI: ", spuri)
+    #
+    # embed =  [
+    #     atproto_client.models.app.bsky.embed.images.Image(
+    #         {"record":{"alt":"THEDingoDave.substack.com BlueSky Liberty", "image":blob_ref,
+    #             "py_type":"app.bsky.embed.images"}}
+    #     )
+    # ]
 
-    print("Blob: ", blob_ref)
-    embed =  [
-        atproto_client.models.AppBskyGraphStarterpack.FeedItem(
-            py_type = "app.bsky.graph.starterpack#feedItem",
-            uri = spuri
-        ),
-        atproto_client.models.AppBskyEmbedImages.Image(
-            alt="THEDingoDave.substack.com BlueSky Liberty",
-            image=blob_ref,
-            py_type="app.bsky.embed.images#image"
-        )
-    ]
     print("Creating Post Record")
 
-    rkey_array = str(blob_ref).split(':')[3].split('/')[2].split("\'")
+#    rkey_array = str(blob_ref).split(':')[3].split('/')[2].split("\'")
+    rkey_array = str(spuri).split(':')[3].split('/')[2].split("\'")
     print("RKEY_ARRAY: ", rkey_array)
     rkey = rkey_array[0]
     print("RKEY: ", rkey)
 
-    bsky_post = client.app.bsky.feed.post.create(
-        repo = did,
-        rkey = rkey,
-        record = {
-            "text":       "Starter Pack by THE Dingo Dave, #" + str(sp_num),
-            "description":"Automated Starter Packs by THE Dingo Dave, #" + str(sp_num),
-            "createdAt":  at_created,
-            "embed":      embed,
-            "py_type":    "app.bsky.feed.post"
+    bsky_post = client.com.atproto.repo.create_record(
+        data = {
+            "repo": did,
+            "collection": "app.bsky.repo.record",
+            "rkey": rkey,
+            "record": {
+                "name":       "Starter Pack by THE Dingo Dave, #" + str(sp_num),
+                "list":       spuri,
+                "description":"Automated Starter Packs by THE Dingo Dave, #" + str(sp_num),
+                "createdAt":  at_created,
+                "py_type":    "app.bsky.graph.starterpack"
+            }
         }
     )
+
     print("Bsky_Post: ", bsky_post)
     return bsky_post
 
-def create_SP(sp_list, spnum, listname, description):
-    client = Client()
-    profile = client.login(handle, password)
+def create_SP(sp_list, spnum, name, description, handle, password):
+    client = session_reuse.init_client(handle, password)
+    # client = Client()
+    # profile = client.login(handle, password)
     resolver = atproto.IdResolver()
     did = resolver.handle.resolve('thedingodave.substack.com')
 
@@ -115,7 +124,7 @@ def create_SP(sp_list, spnum, listname, description):
     aturi = client.app.bsky.graph.list.create(repo=did,
                                               record=models.AppBskyGraphList.Record(
                                                 created_at=at_created,
-                                                name=listname + str(spnum) + "_List",
+                                                name=name + str(spnum) + "_List",
                                                 description=description,
                                                 purpose="app.bsky.graph.defs#referencelist"))
 
@@ -169,7 +178,7 @@ def main():
        all_followers = get_followers()
        # with open('followers.json') as followers_data:
        #     all_followers = json.load(followers_data)
-       create_SP_list(all_followers)
+       create_SP_list(all_followers, bsky_handle, bsky_password)
 
 
 if __name__ == '__main__':
